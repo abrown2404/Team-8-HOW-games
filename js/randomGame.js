688752eb2e8d985be8988276633e22f221f639c3
BRANTEC11 <brandon11leung@gmail.com>
2023-12-12 13:42:55 -0500

    Made changes to header

--- js/randomGame.js
+++ js/randomGame.js
@@ -1,5 +1,5 @@
 document.getElementById('randomGame').addEventListener('click', goToRandomGame);
-const gameArr = ['./ac/ac.html', './BOTW/BOTW.HTML', './EldenRing/EldenRing.Html', "./Adventure/Adventure.html", "./Banjo/Banjo.html", "./morrowwind/morrowind.hmtl", 
+const gameArr = ['./ac/ac.html', './BOTW/BOTW.HTML', './EldenRing/EldenRing.Html', "./Adventure/Adventure.html", "./Banjo/Banjo.html", "./morrowind/morrowind.html", 
 "./fallout3/fallout3.html", "./gta3/gta3.html", "./Mario64/Mario64.html", "./Minecraft/Minecraft.html", "./NoMansSky/NoMansSky.html", "./OcarinaOfTime/OcarinaOfTime.html", 
 "./rdr/rdr.html", "./rdr2/rdr2.html", "./SotN/SotN.html", "./Spiderman/Spiderman.html", "./starfield/starfield.html", "./SuperMetroid/SuperMetroid.html", "./TOTK/TOTK.html", 
 "./Witcher3/Witcher3.html", "./Zelda/Zelda.HTML", "./gta5/gta5.html"];
